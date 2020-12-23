c0d31d472bb03c0a5f298429ddca7ab18d0c76cc
svetlanakovalevskaya <72078444+svetlanakovalevskaya@users.noreply.github.com>
2020-12-23 20:46:27 +0200

    fixed task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,12 +1,16 @@
 const country = prompt("Введите страну доставки:");
-const normalizedCountry = country.toLowerCase();
-let price;
-switch (normalizedCountry) {
-    case "китай": price = 100; break;
-    case "чили": price = 250; break;
-    case "австралия": price = 170; break;
-    case "индия": price = 80; break;
-    case "ямайка": price = 120; break;
-    default : alert("Нет доставки в эту страну!");
+if (country === null) {
+    console.log("Название страны не найдено!");
+} else {
+    const normalizedCountry = country.toLowerCase();
+    let price;
+    switch (normalizedCountry) {
+        case "китай": price = 100; break;
+        case "чили": price = 250; break;
+        case "австралия": price = 170; break;
+        case "индия": price = 80; break;
+        case "ямайка": price = 120; break;
+        default : alert("Нет доставки в эту страну!");
+    }
+    alert(price === undefined ? "Введите другую страну доставки!" : `Доставка в ${country} будет стоить ${price} кредитов`);
 }
-alert(price === undefined ? "Введите другую страну доставки!" : `Доставка в ${country} будет стоить ${price} кредитов`);
